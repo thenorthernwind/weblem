db7e9dd5f61281f75581d9b04fb8274995d572e1
KK <thenorthernwind@rambler.ru>
2016-02-05 12:07:58 +0300

    Конфиг базы данных вынесен в отдельный файл

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,13 @@
 <?php
+$db = array_merge(
+    require(__DIR__ . '/db.php'),
+    require(__DIR__ . '/db-local.php')
+);
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+		'db' => $db,
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
